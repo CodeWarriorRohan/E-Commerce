ed8d15718dd6c729c471898ae06cd643b80ddebc
Rohan Sinodhiya <rohansinodhiya@gmail.com>
2024-08-04 20:21:45 +0530

    second page

---
+++ src/Data/MensKurta.js
@@ -0,0 +1,295 @@
+export const MensKurta=[
+    {
+        "imageUrl":"https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/r/j/4/xl-se-mk-12-sanskrutihomes-original-imahyyy2av25td75.jpeg?q=70",
+        "brand":"Majestic Man",
+        "title":"Men Printed Pure Cotton",
+        "color":"Green",
+        "discountedPrice":"499",
+        "price":"1499",
+        "discountPresent":"66",
+        "size":[
+            {
+                "name":"S",
+                "quantity":20
+            },
+            {
+                "name":"M",
+                "quantity":30
+            },
+            {
+                "name":"L",
+                "quantity":20
+            }
+        ],
+        "quantity":100,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Cloths",
+        "thirsLevelCategory":"Men's Kurta",
+        "description":"A traditional garment enbodying elegance and grace. crafted frome good fabric.",
+    },
+
+    {
+        "imageUrl":"https://rukminim2.flixcart.com/image/612/612/l3es13k0/kurta/t/n/r/l-krmlkbk386-kryptic-original-imagejb7nrphcw2u.jpeg?q=70",
+        "brand":"Majestic Man",
+        "title":"Men Printed Pure Cotton",
+        "color":"Green",
+        "discountedPrice":"499",
+        "price":"1499",
+        "discountPresent":"66",
+        "size":[
+            {
+                "name":"S",
+                "quantity":20
+            },
+            {
+                "name":"M",
+                "quantity":30
+            },
+            {
+                "name":"L",
+                "quantity":20
+            }
+        ],
+        "quantity":100,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Cloths",
+        "thirsLevelCategory":"Men's Kurta",
+        "description":"A traditional garment enbodying elegance and grace. crafted frome good fabric.",
+    },
+
+    {
+        "imageUrl":"https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/6/c/y/s-trendy-men-cutwork-design-red-colour-kurta-increaft-original-imahfnb4akydjy3x.jpeg?q=70",
+        "brand":"Majestic Man",
+        "title":"Men Printed Pure Cotton",
+        "color":"Green",
+        "discountedPrice":"499",
+        "price":"1499",
+        "discountPresent":"66",
+        "size":[
+            {
+                "name":"S",
+                "quantity":20
+            },
+            {
+                "name":"M",
+                "quantity":30
+            },
+            {
+                "name":"L",
+                "quantity":20
+            }
+        ],
+        "quantity":100,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Cloths",
+        "thirsLevelCategory":"Men's Kurta",
+        "description":"A traditional garment enbodying elegance and grace. crafted frome good fabric.",
+    },
+
+    {
+        "imageUrl":"https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/x/y/5/xxl-tk1235-001-tabard-original-imagkn79wsf2ekeg.jpeg?q=70",
+        "brand":"Majestic Man",
+        "title":"Men Printed Pure Cotton",
+        "color":"Green",
+        "discountedPrice":"499",
+        "price":"1499",
+        "discountPresent":"66",
+        "size":[
+            {
+                "name":"S",
+                "quantity":20
+            },
+            {
+                "name":"M",
+                "quantity":30
+            },
+            {
+                "name":"L",
+                "quantity":20
+            }
+        ],
+        "quantity":100,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Cloths",
+        "thirsLevelCategory":"Men's Kurta",
+        "description":"A traditional garment enbodying elegance and grace. crafted frome good fabric.",
+    },
+
+    {
+        "imageUrl":"https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/l/t/y/l-af-mn-red-pri-kur-asian-fitch-original-imagmhfhrrjhhghs.jpeg?q=70",
+        "brand":"Majestic Man",
+        "title":"Men Printed Pure Cotton",
+        "color":"Green",
+        "discountedPrice":"499",
+        "price":"1499",
+        "discountPresent":"66",
+        "size":[
+            {
+                "name":"S",
+                "quantity":20
+            },
+            {
+                "name":"M",
+                "quantity":30
+            },
+            {
+                "name":"L",
+                "quantity":20
+            }
+        ],
+        "quantity":100,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Cloths",
+        "thirsLevelCategory":"Men's Kurta",
+        "description":"A traditional garment enbodying elegance and grace. crafted frome good fabric.",
+    },
+
+    {
+        "imageUrl":"https://rukminim2.flixcart.com/image/612/612/kfvfwy80-0/kurta/r/9/i/l-tk1135-001-tabard-original-imafw8hzadub7wz5.jpeg?q=70",
+        "brand":"Majestic Man",
+        "title":"Men Printed Pure Cotton",
+        "color":"Green",
+        "discountedPrice":"499",
+        "price":"1499",
+        "discountPresent":"66",
+        "size":[
+            {
+                "name":"S",
+                "quantity":20
+            },
+            {
+                "name":"M",
+                "quantity":30
+            },
+            {
+                "name":"L",
+                "quantity":20
+            }
+        ],
+        "quantity":100,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Cloths",
+        "thirsLevelCategory":"Men's Kurta",
+        "description":"A traditional garment enbodying elegance and grace. crafted frome good fabric.",
+    },
+
+    {
+        "imageUrl":"https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/x/y/5/xxl-tk1235-001-tabard-original-imagkn79wsf2ekeg.jpeg?q=70",
+        "brand":"Majestic Man",
+        "title":"Men Printed Pure Cotton",
+        "color":"Green",
+        "discountedPrice":"499",
+        "price":"1499",
+        "discountPresent":"66",
+        "size":[
+            {
+                "name":"S",
+                "quantity":20
+            },
+            {
+                "name":"M",
+                "quantity":30
+            },
+            {
+                "name":"L",
+                "quantity":20
+            }
+        ],
+        "quantity":100,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Cloths",
+        "thirsLevelCategory":"Men's Kurta",
+        "description":"A traditional garment enbodying elegance and grace. crafted frome good fabric.",
+    },
+
+    {
+        "imageUrl":"https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/x/y/5/xxl-tk1235-001-tabard-original-imagkn79wsf2ekeg.jpeg?q=70",
+        "brand":"Majestic Man",
+        "title":"Men Printed Pure Cotton",
+        "color":"Green",
+        "discountedPrice":"499",
+        "price":"1499",
+        "discountPresent":"66",
+        "size":[
+            {
+                "name":"S",
+                "quantity":20
+            },
+            {
+                "name":"M",
+                "quantity":30
+            },
+            {
+                "name":"L",
+                "quantity":20
+            }
+        ],
+        "quantity":100,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Cloths",
+        "thirsLevelCategory":"Men's Kurta",
+        "description":"A traditional garment enbodying elegance and grace. crafted frome good fabric.",
+    },
+
+    {
+        "imageUrl":"https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/x/y/5/xxl-tk1235-001-tabard-original-imagkn79wsf2ekeg.jpeg?q=70",
+        "brand":"Majestic Man",
+        "title":"Men Printed Pure Cotton",
+        "color":"Green",
+        "discountedPrice":"499",
+        "price":"1499",
+        "discountPresent":"66",
+        "size":[
+            {
+                "name":"S",
+                "quantity":20
+            },
+            {
+                "name":"M",
+                "quantity":30
+            },
+            {
+                "name":"L",
+                "quantity":20
+            }
+        ],
+        "quantity":100,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Cloths",
+        "thirsLevelCategory":"Men's Kurta",
+        "description":"A traditional garment enbodying elegance and grace. crafted frome good fabric.",
+    },
+
+    {
+        "imageUrl":"https://rukminim2.flixcart.com/image/612/612/xif0q/kurta/x/y/5/xxl-tk1235-001-tabard-original-imagkn79wsf2ekeg.jpeg?q=70",
+        "brand":"Majestic Man",
+        "title":"Men Printed Pure Cotton",
+        "color":"Green",
+        "discountedPrice":"499",
+        "price":"1499",
+        "discountPresent":"66",
+        "size":[
+            {
+                "name":"S",
+                "quantity":20
+            },
+            {
+                "name":"M",
+                "quantity":30
+            },
+            {
+                "name":"L",
+                "quantity":20
+            }
+        ],
+        "quantity":100,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Cloths",
+        "thirsLevelCategory":"Men's Kurta",
+        "description":"A traditional garment enbodying elegance and grace. crafted frome good fabric.",
+    },
+
+   
+]
+
+export default MensKurta;
--- src/customer/Pages/HomePage/HomePage.jsx
+++ src/customer/Pages/HomePage/HomePage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import HomeCarousel from '../../components/Carousel/HomeCarousel'
+import HomeSectionCarousel from '../../components/HomeSectionCaousel/HomeSectionCarousel'
+import MensKurta from '../../../Data/MensKurta'
 
 const HomePage = () => {
   return (
     <div>
       <HomeCarousel/>
-      <div>
-        Other Section
+      <div className='space-y-10 py-20'>
+        <HomeSectionCarousel data={MensKurta} sectionName={"Men's Kurta"} />
+        <HomeSectionCarousel data={MensKurta} sectionName={"Men's Shoes"}/>
+        <HomeSectionCarousel data={MensKurta} sectionName={"Men's Shirts"}/>
+        <HomeSectionCarousel data={MensKurta} sectionName={"Women's Saree"}/>
+        
       </div>
     </div>
   )
